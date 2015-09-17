674148fcea24a1eb45974020066a790829f0bdc3
stefano nepa <stefano.nepa@outlook.com>
2015-09-17 22:20:45 +0200

    [refactor] tests, with using only mocha (less complex)

---
+++ test/userTest.js
@@ -0,0 +1,11 @@
+﻿var assert = require('assert');
+var request = require('request');
+
+describe('Get user by sciper', function () {
+    it('return userdata in json', function () {
+        request.get('http://localhost:3000/api/public/users/sciper/169419', function (err, res, body) {
+            assert.ok(res.statusCode===200, "Request status 200");
+        });
+    });
+});
+
