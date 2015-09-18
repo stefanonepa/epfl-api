590d6a64b0c9ca0772e45b5d5c3db0cbe758d7e7
nepa <stefano.nepa@epfl.ch>
2015-09-18 11:06:52 +0200

    [fix] mini fix the models (user & unit) [test] fix the access to the property matching with the real object created in the backend

--- test/unitTest.js
+++ test/unitTest.js
@@ -7,15 +7,8 @@ describe('API / Unit', function(){
     // Testing getUnitByName
     it('should be able to read JSON data',function(done){
         request.get('http://localhost:3000/api/public/units/name/igm-ge', function(err, json, headers) {
-            /*
-             * HOW AWFUL IS THAT ?
-             */
-            //console.log(json.body);
-            var chunk = json.body.substring(1,json.body.length-1);
-            //console.log(chunk);
-            var strung = JSON.parse(chunk);
-            //console.log(strung);
-            assert.equal(strung.cn, 'igm-ge', 'Checking cn value');
+            var dataArray = JSON.parse(json.body);
+            assert.equal(dataArray[0].cn, 'igm-ge', 'Checking cn value');
             done();
         });
     });
