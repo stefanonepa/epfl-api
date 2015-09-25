9b66ed05ea2ecafd028707b38e022f468fb578cd
Kebabi boudi <stefano.nepa@outlook.com>
2015-09-25 09:30:49 +0200

    [fix] remove first display of empty list of key generate an strange display [TODO] always an error at the creation of th file (first time)??

--- app/keysManagementController.js
+++ app/keysManagementController.js
@@ -5,23 +5,22 @@
 
         // http://localhost:3000/app/keysManagement/list
         app.get('/', app.tequilaStrategy.ensureAuthenticated, function (req, res) {
-            console.log(req.user.tequila.uniqueid);
-            keysContext.keys.getKeysForUser(req, res,function(keys){
+            keysContext.keys.getKeysForUser(req, res, function(keys) {
                 res.render('keysManagement/list', { keys: keys });
-            })
+            });
         });
 
         app.get('/add', app.tequilaStrategy.ensureAuthenticated, function (req, res) {
-            keysContext.keys.addKeysForUser(req, res,function(keys){
+            keysContext.keys.addKeysForUser(req, res, function (keys) {
                 res.redirect('./');
-            })
+            });
         });
 
         app.get('/delete/:apiKeyToDelete', app.tequilaStrategy.ensureAuthenticated, function (req, res) {
             req.apiKeyToDelete = req.params.apiKeyToDelete;
-            keysContext.keys.deleteKeysForUser(req, res,function(keys){
+            keysContext.keys.deleteKeysForUser(req, res, function (keys) {
                 res.redirect('../');
-            })
+            });
         });
     };
 })(module.exports);
