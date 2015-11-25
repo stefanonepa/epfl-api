5545bc877b5d75cff298a3d2df5eda449f9936bc
nepa <stefano.nepa@epfl.ch>
2015-11-25 16:45:59 +0100

    [refactor] use epfl-ldap 0.4.0

--- api/_handler.js
+++ api/_handler.js
@@ -3,8 +3,13 @@ var render = require('./_render');
 
 module.exports = function handler(req, res, value, routerName, action) {
     if (action.isValid(value)) {
-        req.dataContext[routerName][action.model](value, function (data) {
-            render(req, res, data, routerName);
+        req.dataContext[routerName][action.model](value, function (err, data) {
+            if (err === null) {
+                render(req, res, data, routerName);
+            } else {
+                throw new require('epfl-exceptions').ServerException();
+            }
+            
         });
     } else {
         throw action.validationException;
